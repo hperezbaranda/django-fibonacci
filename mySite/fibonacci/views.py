735d36889eb541e2b991471bfb8e0086ab92e28e
Hector Perez Baranda <ticolotico@gmail.com>
2021-07-29 16:18:14 -0300

    :sparkles: Calculando numero fibonacci

--- mySite/fibonacci/views.py
+++ mySite/fibonacci/views.py
@@ -1,3 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def calculate(request,number):
+    return HttpResponse(fibonacci(number))
+
+def fibonacci(number):
+    if(number == 0 or number == 1):
+        return 1
+    else:
+        return fibonacci(number-1)+fibonacci(number-2)
