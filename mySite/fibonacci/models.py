c1819e09dae0af1a1bafa305c12a0ab482373766
Hector Perez Baranda <ticolotico@gmail.com>
2021-07-29 16:34:12 -0300

    :recycle: arreglando modelos charfields

--- mySite/fibonacci/models.py
+++ mySite/fibonacci/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from django.db.models.fields import CharField
 
 class Fibonacci(models.Model):
-    number = CharField(max_length=50)
-    result = CharField(max_length=50)
+    number = models.CharField(max_length=50)
+    result = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return (self.number,self.result)
--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fibonacci.apps.FibonacciConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
