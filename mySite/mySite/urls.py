8ddfa19e87f00dccd1e31c376f1c2d8e8a1cb8b5
Hector Perez Baranda <ticolotico@gmail.com>
2021-07-29 16:18:42 -0300

    :sparkles: Criando enpoint de aplicacion

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from fibonacci import urls
+from mySite import fibonacci
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('fibonacci',include('fibonacci.urls'))
 ]
